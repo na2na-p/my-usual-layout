f2a8ba53f28207009d722020ecb7efad0adaf5ee
na2na-p <na2na@na2na.dev>
2022-08-09 23:39:22 +0900

    Update eslint Rules

--- dashboard/.eslintrc.cjs
+++ dashboard/.eslintrc.cjs
@@ -1,24 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "google"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'new-cap': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off', // ファイル名とコンポーネント名を一致させることで解決させるので不要
+    'max-len': 'off',
+    'no-tabs': 'off',
+  },
+  'settings': {
+    'react': {
+      version: 'detect',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  },
+};
