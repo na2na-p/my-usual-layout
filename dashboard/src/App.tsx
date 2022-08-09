7fe993ce44b5e50f8e69d60b09ce2886ca844dfe
na2na-p <na2na@na2na.dev>
2022-08-09 23:39:15 +0900

    WIP

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,30 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+/* eslint-disable require-jsdoc */
 
 function App() {
-  const [count, setCount] = useState(0)
+  const currentPath = window.location.pathname;
 
   return (
-	<>
-	<p>
-		Hello, I'm one of the panels in your bundle!
-		I'm where you put your controls.
-	</p>
+    <>
+      <p>
+		Hello, I&apos;m one of the panels in your bundle!
+		I&apos;m where you put your controls.
+      </p>
 
-	<p>To edit me, open "<span id="path" className="monospace"></span>" in your favorite text editor or IDE.</p>
+      <p>To edit me, open &quot;<span id="path" className="monospace">{currentPath}</span>&quot; in your favorite text editor or IDE.</p>
 
-	<p>
+      <p>
 		You can use any libraries, frameworks, and tools you want.
 		The only limit of panels is that they cannot render outside of their
-		<span className="monospace">&lt;iframe&gt;</span>'s bounding box.
-	</p>
+        <span className="monospace">&lt;iframe&gt;</span>&apos;s bounding box.
+      </p>
 
-	<p>Visit <a href="https://nodecg.com" target="_blank" rel="noopener">https://nodecg.com</a> for full documentation.</p>
+      <p>Visit <a href="https://nodecg.com" target="_blank" rel="noopener noreferrer">https://nodecg.com</a> for full documentation.</p>
 
-	<p>You've got this!</p>
-	</>
-  )
+      <p>You&apos;ve got this!</p>
+    </>
+  );
 }
 
-export default App
+export default App;
