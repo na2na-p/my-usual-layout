c99d18cbbe9b70cc19ad87b4753bda1752c9b8f1
na2na-p <na2na@na2na.dev>
2022-08-10 00:03:42 +0900

    Update とりあえずデモと同じ画面が出るように

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,28 +1,31 @@
-/* eslint-disable require-jsdoc */
+/// <reference path="../../../../types/browser.d.ts" />
 
+// eslint-disable-next-line require-jsdoc
 function App() {
-  const currentPath = window.location.pathname;
+	const currentPath = window.location.pathname;
+	// ロード完了時にnodecg.log.info('Here\'s an example of using NodeCG\'s logging API!');する
+	nodecg.log.info('Here\'s an example of using NodeCG\'s logging API!');
 
-  return (
-    <>
-      <p>
+	return (
+		<>
+			<p>
 		Hello, I&apos;m one of the panels in your bundle!
 		I&apos;m where you put your controls.
-      </p>
+			</p>
 
-      <p>To edit me, open &quot;<span id="path" className="monospace">{currentPath}</span>&quot; in your favorite text editor or IDE.</p>
+			<p>To edit me, open &quot;<span id="path" className="monospace">{currentPath}</span>&quot; in your favorite text editor or IDE.</p>
 
-      <p>
+			<p>
 		You can use any libraries, frameworks, and tools you want.
 		The only limit of panels is that they cannot render outside of their
-        <span className="monospace">&lt;iframe&gt;</span>&apos;s bounding box.
-      </p>
+				<span className="monospace">&lt;iframe&gt;</span>&apos;s bounding box.
+			</p>
 
-      <p>Visit <a href="https://nodecg.com" target="_blank" rel="noopener noreferrer">https://nodecg.com</a> for full documentation.</p>
+			<p>Visit <a href="https://nodecg.com" target="_blank" rel="noopener noreferrer">https://nodecg.com</a> for full documentation.</p>
 
-      <p>You&apos;ve got this!</p>
-    </>
-  );
+			<p>You&apos;ve got this!</p>
+		</>
+	);
 }
 
 export default App;
