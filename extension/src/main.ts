a1416bb51587f4ed7c3992595b317767b67a067c
na2na-p <na2na@na2na.dev>
2022-08-10 21:50:29 +0900

    Update

--- extension/src/main.ts
+++ extension/src/main.ts
@@ -1,5 +1,32 @@
-// Directly import the type definition file
-import {NodeCG} from '@nodecg';
+type ReplicantMap = {
+	misskeyNotes: Array<{name:string, context: string}>
+	players: Array<{name: string; twitter: string}>;
+	// ...
+};
+
+type CustomError = unknown
+
+type MessageMap = {
+	updateFoo: {
+		// optional value to send with sendMessage / value to receive from listenFor
+		data: number;
+		// optional value to receive from sendMessage / value to send with listenFor callback
+		result: number;
+		// optional error object/value sent from listenFor callback
+		error: CustomError;
+	};
+};
+
+type BundleConfig = any;
+
+import {CreateNodecgInstance} from 'ts-nodecg/server';
+
+type NodeCG = CreateNodecgInstance<
+	'my-usual-layout', // name of your bundle
+	BundleConfig,
+	ReplicantMap,
+	MessageMap
+>;
 
 export default (nodecg: NodeCG) => {
 	nodecg.log.info('Hello, from your bundle\'s extension!');
