85643477519c3b1cc90ab1cbe3cde3cf0d5b2fbf
na2na-p <na2na@na2na.dev>
2022-08-11 00:07:43 +0900

    Update

--- src/dashboard/vite.config.ts
+++ src/dashboard/vite.config.ts
@@ -1,10 +1,18 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
+const MODE = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-	base: `/bundles/my-usual-layout/dashboard/`,
+	base: (() => {
+		if (MODE === 'development') {
+			return './';
+		} else {
+			return `/bundles/my-usual-layout/graphics/`;
+		}
+	})(),
 	build: {
 		outDir: '../../dashboard',
 		emptyOutDir: true,
